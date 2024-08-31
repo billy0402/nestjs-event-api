c7182679b8684eeca59d901c8e9ff653ef84479f
Billy Huang <10646003@ntub.edu.tw>
2024-08-31 11:47:57 +0800

    feat(admin-events): controller add auth guard and roles guard

--- src/apps/admin-events/admin-events.controller.ts
+++ src/apps/admin-events/admin-events.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Put,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiCreatedResponse,
@@ -16,14 +17,19 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 
+import { Role } from '@prisma/client';
 import { Response } from 'express';
 import { ZodSerializerDto } from 'nestjs-zod';
 
 import { EventInDto, EventOutDto } from '@/dto/event.dto';
+import { AuthGuard } from '@/guards/auth/auth.guard';
+import { Roles, RolesGuard } from '@/guards/roles/roles.guard';
 
 import { AdminEventsService } from './admin-events.service';
 
 @ApiTags('admin-events')
+@Roles([Role.ADMIN])
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('admin/events')
 export class AdminEventsController {
   constructor(private readonly adminEventService: AdminEventsService) {}
--- src/apps/admin-events/admin-events.module.ts
+++ src/apps/admin-events/admin-events.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { PrismaService } from '@/services/prisma/prisma.service';
+import { UsersService } from '@/services/users/users.service';
 
 import { AdminEventsController } from './admin-events.controller';
 import { AdminEventsService } from './admin-events.service';
 
 @Module({
+  imports: [JwtModule],
   controllers: [AdminEventsController],
-  providers: [AdminEventsService, PrismaService],
+  providers: [AdminEventsService, PrismaService, UsersService],
 })
 export class AdminEventsModule {}
